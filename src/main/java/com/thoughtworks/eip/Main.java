133d7abe73117db5cfc919290ff1e165d4589003
Marcos Silveira <bymarkone@gmail.com>
2016-09-09 10:04:01 -0300

    Initial setup

---
+++ src/main/java/com/thoughtworks/eip/Main.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.eip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:integration.xml")
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/java/com/thoughtworks/eip/ProjectSimulator.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.eip;
+
+public interface ProjectSimulator {
+}
